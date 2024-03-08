f2633ac70a64fa730f1b362e7963296f02d084f5
Arian Baishya <arian.baishya2004@gmail.com>
2024-03-08 16:19:31 -0600

    (Arduino/Motors) fix build errors and formatting

--- arduino/firmware.cpp
+++ arduino/firmware.cpp
@@ -1,5 +1,4 @@
 // Include Libraries
-#include <Arduino.h>
 #include "DCMDriverL298.h"
 
 // Pin Definitions
@@ -17,31 +16,30 @@
 #define MOTORDRIVER_2_PIN_INT3	12
 #define MOTORDRIVER_2_PIN_INT4	13
 
-
 DCMDriverL298 frontMotorDriver(
-	MOTORDRIVERL298_1_PIN_ENA,
-	MOTORDRIVERL298_1_PIN_INT1,
-	MOTORDRIVERL298_1_PIN_INT2,
-	MOTORDRIVERL298_1_PIN_ENB,
-	MOTORDRIVERL298_1_PIN_INT3,
-	MOTORDRIVERL298_1_PIN_INT4
+	MOTORDRIVER_1_PIN_ENA,
+	MOTORDRIVER_1_PIN_INT1,
+	MOTORDRIVER_1_PIN_INT2,
+	MOTORDRIVER_1_PIN_ENB,
+	MOTORDRIVER_1_PIN_INT3,
+	MOTORDRIVER_1_PIN_INT4
 );
 DCMDriverL298 rearMotorDriver(
-	MOTORDRIVERL298_2_PIN_ENA,
-	MOTORDRIVERL298_2_PIN_INT1,
-	MOTORDRIVERL298_2_PIN_INT2,
-	MOTORDRIVERL298_2_PIN_ENB,
-	MOTORDRIVERL298_2_PIN_INT3,
-	MOTORDRIVERL298_2_PIN_INT4
+	MOTORDRIVER_2_PIN_ENA,
+	MOTORDRIVER_2_PIN_INT1,
+	MOTORDRIVER_2_PIN_INT2,
+	MOTORDRIVER_2_PIN_ENB,
+	MOTORDRIVER_2_PIN_INT3,
+	MOTORDRIVER_2_PIN_INT4
 );
 
 // TODO: make code below actually use these constants
 // motor constants
-const int MAX_SPEED = 0
-const int NORMAL_SPEED = 0
-const int OFF_SPEED = 0
-const int FORWARD = 0 // TODO: figure out if this is correct
-const int BACKWARD = 1 // TODO: figure out if this is correct
+const int MAX_SPEED = 0;
+const int NORMAL_SPEED = 0;
+const int OFF_SPEED = 0;
+const int FORWARD = 0; // TODO: figure out if this is correct
+const int BACKWARD = 1; // TODO: figure out if this is correct
 
 // define vars for testing menu
 const int TIMEOUT = 10000;
@@ -58,18 +56,14 @@ char menu() {
 	while (!Serial.available());
 
 	// Read data from serial monitor if received
-	while (Serial.available())
-	{
+	while (Serial.available()) {
 		char c = Serial.read();
-		if (isAlphaNumeric(c))
-		{
-
-			if(c == '1')
+		if (isAlphaNumeric(c)) {
+			if(c == '1') {
 				Serial.println(F("Now Testing L298N Motor Driver with Dual Hobby DC motors #1"));
-				else if(c == '2')
+			} else if(c == '2') {
 				Serial.println(F("Now Testing L298N Motor Driver with Dual Hobby DC motors #2"));
-			else
-			{
+			} else {
 				Serial.println(F("illegal input!"));
 				return 0;
 			}
@@ -90,30 +84,28 @@ void setup() {
 
 void loop() {
 	//if(menuOption == '1') {
-	// L298N Motor Driver with Dual Hobby DC motors #1 - Test Code
-	// Start both motors. note that rotation direction is determined by the motors connection to the driver.
-	// You can change the speed by setting a value between 0-255, and set the direction by changing between 1 and 0.
-	frontMotorDriver.setMotorA(200,1);
-	frontMotorDriver.setMotorB(200,0);
-	delay(2000);
+		// Start both motors. note that rotation direction is determined by the motors connection to the driver.
+		// You can change the speed by setting a value between 0-255, and set the direction by changing between 1 and 0.
+		frontMotorDriver.setMotorA(200,1);
+		frontMotorDriver.setMotorB(200,0);
+		delay(2000);
 
-	//Stop both motors
-	frontMotorDriver.stopMotors();
-	delay(2000);
+		//Stop both motors
+		frontMotorDriver.stopMotors();
+		delay(2000);
 
 	//} else if(menuOption == '2') {
-	rearMotorDriver.setMotorA(200,1);
-	rearMotorDriver.setMotorB(200,0);
-	delay(2000);
+		rearMotorDriver.setMotorA(200,1);
+		rearMotorDriver.setMotorB(200,0);
+		delay(2000);
 
-	//Stop both motors
-	rearMotorDriver.stopMotors();
-	delay(2000);
-	//
+		//Stop both motors
+		rearMotorDriver.stopMotors();
+		delay(2000);
+		//
 	//}
 
 	//if (millis() - time0 > TIMEOUT) {
-	//menuOption = menu();
+		//menuOption = menu();
 	//}
-
 }
