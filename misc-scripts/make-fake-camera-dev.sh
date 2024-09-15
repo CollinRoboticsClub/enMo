847ae050014a9e0251555ce19266d8bc45475d2c
Arian Baishya <arian.baishya2004@gmail.com>
2024-09-15 14:01:14 -0500

    (misc-scripts): allow fake-camera script to take video file path as argument

--- misc-scripts/make-fake-camera-dev.sh
+++ misc-scripts/make-fake-camera-dev.sh
@@ -4,5 +4,5 @@
 # You should only need to do it once until you reboot unless you `modprobe -r` it.
 
 # create fake camera device and stream video to it
-VIDEO_FILE_PATH="$HOME/Videos/numbered-frames.mkv"
+VIDEO_FILE_PATH="${1:-$HOME/Videos/numbered-frames.mkv}"
 ffmpeg -re -i "$VIDEO_FILE_PATH" -s 1280x720 -vcodec rawvideo -pix_fmt yuv420p -f v4l2 /dev/video0
